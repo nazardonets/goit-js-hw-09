267f74a6f1c57ecf4250566fb6cf687060ecf4d1
Nazar D <nazardonets@gmail.com>
2022-10-06 01:04:32 +0300

    fixed a bug with initial delay in task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,12 +5,11 @@ const formEl = document.querySelector('.form');
 const inputDelayEl = formEl.elements['delay'];
 const inputStepEl = formEl.elements['step'];
 const inputAmountEl = formEl.elements['amount'];
-const submitBtnEl = formEl.querySelector('button[type="submit"]');
 
 function createPromise(position, delay) {
    const shouldResolve = Math.random() > 0.5;
 
-   const promise = new Promise((resolve, reject) => {
+   return new Promise((resolve, reject) => {
       setTimeout(() => {
          if (shouldResolve) {
             resolve({ position, delay });
@@ -25,23 +24,19 @@ function createPromise(position, delay) {
       .catch(({ position, delay }) => {
          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-
-   return promise;
 }
 
-formEl.addEventListener('click', e => {
+formEl.addEventListener('submit', e => {
    e.preventDefault();
 
-   if (e.target === submitBtnEl) {
-      let numberOfRepeats = parseInt(inputAmountEl.value);
-      let step = parseInt(inputStepEl.value);
+   let numberOfRepeats = parseInt(inputAmountEl.value);
+   let step = parseInt(inputStepEl.value);
 
-      for (i = 1; i <= numberOfRepeats; i++) {
-         if (i === 1) {
-            step = parseInt(inputDelayEl.value);
-         }
-         createPromise(i, step);
-         step += parseInt(inputStepEl.value);
+   for (i = 1; i <= numberOfRepeats; i++) {
+      if (i === 1) {
+         step = parseInt(inputDelayEl.value);
       }
+      createPromise(i, step);
+      step += parseInt(inputStepEl.value);
    }
 });
